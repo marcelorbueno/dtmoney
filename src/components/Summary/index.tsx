afbcf2aa1373710d43ea777efec2b0f3c50b1733
Marcelo Bueno <marcelorezendebueno@gmail.com>
2021-04-22 19:35:48 -0300

    Criando hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,12 +1,13 @@
-import { useContext } from 'react'
+import { useTransactions } from '../../hooks/useTransactions';
+
 import IncomeImg from '../../assets/income.svg'
 import OutcomeImg from '../../assets/outcome.svg'
 import TotalImg from '../../assets/total.svg'
+
 import { Container } from "./styles";
-import { TransactionsContext } from '../../TransactionsContext';
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext);
+    const { transactions } = useTransactions();
 
     const summary = transactions.reduce((acc, transaction) => {
         if (transaction.type === 'deposit') {
